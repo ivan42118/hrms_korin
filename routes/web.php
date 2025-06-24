edf51286be29d327786abef1bc7fa026bac5d200
ivan42118 <ivan42118@gmail.com>
2025-06-24 07:45:36 +0700

    Test

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DivisionController;
 use App\Http\Controllers\EmployeeController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::resource('employees', EmployeeController::class);
-Route::resource('divisions', DivisionController::class);
-Route::get('/employees/create', [EmployeeController::class, 'create'])->name('employees.create');
+// Tambahkan route untuk divisions dan employees
+Route::middleware(['auth'])->group(function () {
+    Route::resource('divisions', DivisionController::class);
+    Route::resource('employees', EmployeeController::class);
+});
+
+require __DIR__.'/auth.php';
