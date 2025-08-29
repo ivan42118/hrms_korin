f39968cf2cb23dd52537b82ea50d6a7b32924b7a
ivan42118 <ivan42118@gmail.com>
2025-08-29 08:34:08 +0700

    Dari 0 lagi

--- resources/views/divisions/create.blade.php
+++
@@ -1,9 +0,0 @@
-<h2>Tambah Divisi</h2>
-<form method="POST" action="{{ route('divisions.store') }}">
-    @csrf
-    <input type="text" name="nama_divisi" placeholder="Nama Divisi" required>
-    <button type="submit">Simpan</button>
-</form>
-<a href="{{ route('divisions.index') }}">← Kembali</a>
-
-
--- resources/views/divisions/edit.blade.php
+++
@@ -1,8 +0,0 @@
-<h2>Edit Divisi</h2>
-<form method="POST" action="{{ route('divisions.update', $division) }}">
-    @csrf
-    @method('PUT')
-    <input type="text" name="nama_divisi" value="{{ $division->nama_divisi }}" required>
-    <button type="submit">Update</button>
-</form>
-<a href="{{ route('divisions.index') }}">← Kembali</a>
